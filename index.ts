600731bb41c7edaa2ee14304d0fd80ddf4316bd7
sdnshu <sudhanshuneravati077@gmail.com>
2025-10-06 10:14:09 +0530

    feat: add /api/auth route

--- index.ts
+++ index.ts
@@ -1,8 +1,12 @@
 import { Hono } from 'hono'
 
+import { auth } from '@/lib/auth';
+
 const app = new Hono()
 
 app.get('/', (c) => { return c.text('Hello Hono!') })
+
+app.on(["POST", "GET"], "/api/auth/*", (c) => auth.handler(c.req.raw));
 
 export default {
     fetch: app.fetch,
