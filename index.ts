1893890ba586b19526fbbbd088e7dcd97e4a9b26
sdnshu <sudhanshuneravati077@gmail.com>
2025-10-06 10:30:25 +0530

    feat: add cors

--- index.ts
+++ index.ts
@@ -1,8 +1,21 @@
 import { Hono } from 'hono'
+import { cors } from "hono/cors";
 
 import { auth } from '@/lib/auth';
 
 const app = new Hono()
+
+app.use(
+    "/api/auth/*",
+    cors({
+        origin: "http://localhost:3000",
+        allowHeaders: ["Content-Type", "Authorization"],
+        allowMethods: ["POST", "GET", "OPTIONS"],
+        exposeHeaders: ["Content-Length"],
+        maxAge: 600,
+        credentials: true,
+    }),
+);
 
 app.get('/', (c) => { return c.text('Hello Hono!') })
 
