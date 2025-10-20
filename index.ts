6ee8c2ccc0bc3ea6d1638a14ec146b94ecdb83eb
sdnshu <sudhanshuneravati055@gmail.com>
2025-10-20 22:30:40 +0530

    feat(auth): initialize better-auth

--- index.ts
+++ index.ts
@@ -1,7 +1,33 @@
 import { Hono } from 'hono'
+import { cors } from "hono/cors";
+import { etag } from 'hono/etag'
+import { logger } from 'hono/logger'
+import { poweredBy } from 'hono/powered-by'
+
+import { auth } from '@/lib/auth';
 
 const app = new Hono()
 
+app.use(poweredBy())
+app.use(etag(), logger())
+
+app.use(
+    "/api/auth/*",
+    cors({
+        origin: "http://localhost:3000",
+        allowHeaders: ["Content-Type", "Authorization"],
+        allowMethods: ["POST", "GET", "OPTIONS"],
+        exposeHeaders: ["Content-Length"],
+        maxAge: 600,
+        credentials: true,
+    }),
+);
+
 app.get('/', (c) => { return c.text('Hello Hono!') })
 
-export default app
+app.on(["POST", "GET"], "/api/auth/*", (c) => auth.handler(c.req.raw));
+
+export default {
+    fetch: app.fetch,
+    port: 4444,
+}
